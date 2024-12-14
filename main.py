911f305d39164179f104059ac69fbd686ab69335
Nayy-dev <inayahahwi098@gmail.com>
2024-12-15 03:46:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-python -m pip install --upgrade pip
 import streamlit as st
 import pandas as pd
 from langdetect import detect
@@ -9,6 +8,7 @@ import logging
 import os
 import aiohttp
 from github import Github
+python -m pip install --upgrade pip
 
 # Konfigurasi logging
 logging.basicConfig(
